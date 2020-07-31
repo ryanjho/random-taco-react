1f4f3374febc2c2e38cad7f3d755d3a125ea8a35
Ryan Ho <ryanjho90@gmail.com>
2020-07-31 16:33:40 +0800

    Updates

---
+++ precache-manifest.109db48df81c71c62e51398e33eea7a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06919a4a8917b846385a390d8d5983b9",
+    "url": "/random-taco-react/index.html"
+  },
+  {
+    "revision": "28b9b9bfd59fcbc47eb6",
+    "url": "/random-taco-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d096354bc85a2ac0b15",
+    "url": "/random-taco-react/static/js/2.bb41ca11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-taco-react/static/js/2.bb41ca11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b9b9bfd59fcbc47eb6",
+    "url": "/random-taco-react/static/js/main.7e26bd4f.chunk.js"
+  },
+  {
+    "revision": "52871978aaeae62052b5",
+    "url": "/random-taco-react/static/js/runtime-main.ad683ded.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-taco-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
